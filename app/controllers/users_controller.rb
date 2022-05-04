e6dd3dad853ce24891e352497164c36ed27a79dd
carolinamorah <carolinamorahernandez@hotmail.com>
2022-05-04 12:55:57 -0400

    More fixes to sengrid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,12 @@
 class UsersController < ApplicationController
     
-    def create
-        # Create the user from params
-        @user = User.new(user_params)
-        if @user.save
-          # Deliver the signup email
-          UserNotifierMailer.send_signup_email(@user).deliver
-          redirect_to(@user, :notice => 'User created')
-        else
-          render :action => 'new'
-        end
-    end
-
-
     def show
         @user = User.find(params[:id])
     end
 
     private
 
-    def user_params
-    params.require(:user).permit(:name, :email, :login)
-    end
+
     
 
     
